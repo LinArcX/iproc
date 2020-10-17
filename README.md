272b67cc28c0b3167db199f676774666ad7dddc0
linarcx <linarcx@gmail.com>
2020-10-17 11:16:39 +0330

    Update readme file that contains instructions to build for windows users

--- README.md
+++ README.md
@@ -28,22 +28,37 @@ You'll see something like this:
 To see full range of options, use `-h`:
 ```
 Options:
-        -p,--process-id  Specify the process-id to watch
-        -i,--interval   Specify interval
-        -h,--help       Show this help message
+        -p,--process-id     The process-id to watch
+        -i,--interval       The period of time to do unlimited calculations.(default = 2)
+        -o,--one-shot       Just calculate once
+        -r,--repetition     The number of repetitions
+        -h,--help           Show this help message
 ```
 
 # Build
 ## Linux
 First make sure you have these tools on your machine:
 - `make`
-- `clang` or `gcc`
+- `clang` or `gcc` or any other c-compiler that you prefre.
 
 Then:
 `cd linux; make; ./release/iproc -p <PID>`
 
 ## Windows
-TODO
+You have three options:
+1. Open solution file that resides here: `.\windows\visual_studio\iproc.sln` and build the project through IDE.
+2. Build from command-line via `cl.exe`.
+    2.1. Open visual studio command prompt.
+    2.3. Change to __.\iproc\windows__ direcotry.
+    2.4. `cl.exe ..\core\iproc.c .\windows.c`
+
+    Tip: If you don't have visual-studio command prompt or prefer to stay in your current pwsh, you can prepare `cl.exe` via this command:
+    `Invoke-CmdScript "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"`
+3. Or you can use __mingw__(that contains **gcc__), or __clang__ or any other c-compiler that you prefer.
+
+# Todo
+- [] Cpu usage in linux isn't correct.
+- [] Memory usage in windows isn't correct.
 
 ## License
 ![License](https://img.shields.io/github/license/LinArcX/iproc.svg)
--- core/iproc.c
+++ core/iproc.c
@@ -10,133 +10,127 @@
 #include "../linux/linux.h"
 #endif
 
-void
-show_usage(char *name)
-{
-    fprintf(stdout,
-            "Usage: %s\n"
-            "Options:\n"
-            "\t-p,--process-id \tThe process-id to watch\n"
-            "\t-i,--interval \tThe period of time to do unlimited "
-            "calculations.(default = 2)\n"
-            "\t-o,--one-shot \tJust calculate once\n"
-            "\t-r,--repetition \tThe number of repetitions\n"
-            "\t-h,--help \tShow this help message\n",
-            name);
+void show_usage(char *name) {
+  fprintf(stdout,
+          "Usage: %s\n"
+          "Options:\n"
+          "\t-p,--process-id \tThe process-id to watch\n"
+          "\t-i,--interval \tThe period of time to do unlimited "
+          "calculations.(default = 2)\n"
+          "\t-o,--one-shot \tJust calculate once\n"
+          "\t-r,--repetition \tThe number of repetitions\n"
+          "\t-h,--help \tShow this help message\n",
+          name);
 }
 
-int
-main(int argc, char **argv)
-{
-    // -p is necessary!
-    if (argc < 2) {
-        show_usage(argv[0]);
+int main(int argc, char **argv) {
+  // -p is necessary!
+  if (argc < 2) {
+    show_usage(argv[0]);
+    return 1;
+  }
+
+  int pid = 0;
+  int elapsed = 0;
+
+  int interval = 0;
+  int one_shot = 0;
+  int repetition = 0;
+
+  for (int i = 1; i < argc; ++i) {
+    char *arg = argv[i];
+    if (strcmp(arg, "-h") == 0 || strcmp(arg, "--help") == 0) {
+      show_usage(argv[0]);
+      return 0;
+    } else if (strcmp(arg, "-p") == 0 || strcmp(arg, "--process-id") == 0) {
+      if (i + 1 < argc) {  // Make sure we aren't at the end of argv!
+        pid = atoi(argv[++i]);
+      } else {  // Uh-oh, there was no argument to the destination option.
+        fprintf(stderr, "--process-id option requires one argument.");
         return 1;
+      }
+    } else if (strcmp(arg, "-i") == 0 || strcmp(arg, "--interval") == 0) {
+      if (i + 1 < argc) {
+        interval = atoi(argv[++i]);
+      } else {
+        fprintf(stderr, "--interval option requires one argument.");
+        return 1;
+      }
+    } else if (strcmp(arg, "-o") == 0 || strcmp(arg, "--one-shot") == 0) {
+      one_shot = 1;
+    } else if (strcmp(arg, "-r") == 0 || strcmp(arg, "--repetition") == 0) {
+      if (i + 1 < argc) {
+        repetition = atoi(argv[++i]);
+      } else {
+        fprintf(stderr, "--repetition option requires one argument.");
+        return 1;
+      }
     }
+  }
 
-    int pid = 0;
-    int elapsed = 0;
+  if (pid) {
+    if (interval) {
+      for (;;) {
+#ifdef WIN32
+        elapsed += interval;
+        Sleep(interval * 1000);
 
-    int interval = 0;
-    int one_shot = 0;
-    int repetition = 0;
+        printf("[%d]\n\tMem: %d Kb\n", elapsed, memory_usage(pid));
+        printf("\tCpu: %d%%\n", cpu_usage(pid));
+#elif __linux__
+        elapsed += interval;
+        sleep(interval);
 
-    for (int i = 1; i < argc; ++i) {
-        char *arg = argv[i];
-        if (strcmp(arg, "-h") == 0 || strcmp(arg, "--help") == 0) {
-            show_usage(argv[0]);
-            return 0;
-        } else if (strcmp(arg, "-p") == 0 || strcmp(arg, "--process-id") == 0) {
-            if (i + 1 < argc) { // Make sure we aren't at the end of argv!
-                pid = atoi(argv[++i]);
-            } else { // Uh-oh, there was no argument to the destination option.
-                fprintf(stderr, "--process-id option requires one argument.");
-                return 1;
-            }
-        } else if (strcmp(arg, "-i") == 0 || strcmp(arg, "--interval") == 0) {
-            if (i + 1 < argc) {
-                interval = atoi(argv[++i]);
-            } else {
-                fprintf(stderr, "--interval option requires one argument.");
-                return 1;
-            }
-        } else if (strcmp(arg, "-o") == 0 || strcmp(arg, "--one-shot") == 0) {
-            one_shot = 1;
-        } else if (strcmp(arg, "-r") == 0 || strcmp(arg, "--repetition") == 0) {
-            if (i + 1 < argc) {
-                repetition = atoi(argv[++i]);
-            } else {
-                fprintf(stderr, "--repetition option requires one argument.");
-                return 1;
-            }
-        }
+        cpu_init();
+        printf("[%d]\n\tMem: %d Kb\n", elapsed, memory_usage(pid));
+        printf("\tCpu: %f%%\n", cpu_usage());
+#endif
+      }
+    } else if (one_shot) {
+#ifdef WIN32
+      printf("[%d]\n\tMem: %d Kb\n", elapsed, memory_usage(pid));
+      printf("\tCpu: %d%%\n", cpu_usage(pid));
+      // system("ps -o\"% C\" -p 30747");
+#elif __linux__
+      cpu_init();
+      printf("[%d]\n\tMem: %d Kb\n", elapsed, memory_usage(pid));
+      printf("\tCpu: %f%%\n", cpu_usage());
+#endif
+    } else if (repetition) {
+      for (int i = 0; i < repetition; i++) {
+#ifdef WIN32
+        elapsed += 2;
+        Sleep(2000);
+
+        printf("[%d]\n\tMem: %d Kb\n", elapsed, memory_usage(pid));
+        printf("\tCpu: %d%%\n", cpu_usage(pid));
+#elif __linux__
+        elapsed += 2;
+        sleep(2);
+
+        cpu_init();
+        printf("[%d]\n\tMem: %d Kb\n", elapsed, memory_usage(pid));
+        printf("\tCpu: %f%%\n", cpu_usage());
+#endif
+      }
+    } else {
+      for (;;) {
+#ifdef WIN32
+        elapsed += 2;
+        Sleep(2000);
+
+        printf("[%d]\n\tMem: %d Kb\n", elapsed, memory_usage(pid));
+        printf("\tCpu: %d%%\n", cpu_usage(pid));
+#elif __linux__
+        elapsed += 2;
+        sleep(2);
+
+        cpu_init();
+        printf("[%d]\n\tMem: %d Kb\n", elapsed, memory_usage(pid));
+        printf("\tCpu: %f%%\n", cpu_usage());
+#endif
+      }
     }
-
-    if (pid) {
-        if (interval) {
-            for (;;) {
-#ifdef WIN32
-                elapsed += interval;
-                Sleep(interval * 1000);
-
-                printf("[%d]\n\tMem: %d Kb\n", elapsed, memory_usage(pid));
-                printf("\tCpu: %d%%\n", cpu_usage(pid));
-#elif __linux__
-                elapsed += interval;
-                sleep(interval);
-
-                cpu_init();
-                printf("[%d]\n\tMem: %d Kb\n", elapsed, memory_usage(pid));
-                printf("\tCpu: %f%%\n", cpu_usage());
-#endif
-            }
-        } else if (one_shot) {
-#ifdef WIN32
-            printf("[%d]\n\tMem: %d Kb\n", elapsed, memory_usage(pid));
-            printf("\tCpu: %d%%\n", cpu_usage(pid));
-            system("ps -o\"% C\" -p 30747");
-#elif __linux__
-            cpu_init();
-            printf("[%d]\n\tMem: %d Kb\n", elapsed, memory_usage(pid));
-            printf("\tCpu: %f%%\n", cpu_usage());
-#endif
-        } else if (repetition) {
-            for (int i = 0; i < repetition; i++) {
-#ifdef WIN32
-                elapsed += 2;
-                Sleep(2000);
-
-                printf("[%d]\n\tMem: %d Kb\n", elapsed, memory_usage(pid));
-                printf("\tCpu: %d%%\n", cpu_usage(pid));
-                system("ps -o\"% C\" -p 30747");
-#elif __linux__
-                elapsed += 2;
-                sleep(2);
-
-                cpu_init();
-                printf("[%d]\n\tMem: %d Kb\n", elapsed, memory_usage(pid));
-                printf("\tCpu: %f%%\n", cpu_usage());
-#endif
-            }
-        } else {
-            for (;;) {
-#ifdef WIN32
-                elapsed += 2;
-                Sleep(2000);
-
-                printf("[%d]\n\tMem: %d Kb\n", elapsed, memory_usage(pid));
-                printf("\tCpu: %d%%\n", cpu_usage(pid));
-                system("ps -o\"% C\" -p 30747");
-#elif __linux__
-                elapsed += 2;
-                sleep(2);
-
-                cpu_init();
-                printf("[%d]\n\tMem: %d Kb\n", elapsed, memory_usage(pid));
-                printf("\tCpu: %f%%\n", cpu_usage());
-#endif
-            }
-        }
-    }
-    return 0;
+  }
+  return 0;
 }
